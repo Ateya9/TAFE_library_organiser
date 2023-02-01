da1e01c492c47033aa6c0b57192d5090c262abb4
Dat Ngo <dat_ngo10@protonmail.com>
2023-02-01 14:07:09 +0800

    feat: Added a add book feature

--- js/script.js
+++ js/script.js
@@ -1,14 +1,54 @@
-const saveButton = document.querySelector('')
+const saveButton = document.querySelector('[data-save]')
+const bookList = document.querySelector('.list')
+const title = document.querySelector('#title')
+const author = document.querySelector('#author')
+const genre = document.querySelector('#genre')
+const isbn = document.querySelector('#isbn')
 
-const book = document.createElement('div')
-const title = document.createElement('p')
-const author = document.createElement('p')
-const genre = document.createElement('p')
-const isbn = document.createElement('p')
+const bookArray = []
 
-book.classList.add('book')
+function createBookObj(title, author, genre, isbn) {
+	return {
+		title,
+		author,
+		genre,
+		isbn
+	}
+}
 
-book.appendChild(title)
-book.appendChild(author)
-book.appendChild(genre)
-book.appendChild(isbn)
+function render(books) {
+	bookList.innerHTML = ''
+
+	books.forEach( ({ title, author, genre, isbn }) => {
+		const bookElement = document.createElement('div')
+		const bookTitle = document.createElement('p')
+		bookTitle.textContent = title
+		const bookAuthor = document.createElement('p')
+		bookAuthor.textContent = author
+		const bookGenre = document.createElement('p')
+		bookGenre.textContent = genre
+		const bookIsbn = document.createElement('p')
+		bookIsbn.textContent = isbn
+		bookElement.classList.add('book')
+
+		bookElement.appendChild(bookTitle)
+		bookElement.appendChild(bookAuthor)
+		bookElement.appendChild(bookGenre)
+		bookElement.appendChild(bookIsbn)
+		bookList.appendChild(bookElement)
+	});
+} 
+
+saveButton.addEventListener('click', (e) => {
+	e.preventDefault()
+	bookArray.push(createBookObj(title.value, author.value, genre.value, isbn.value))
+	render(bookArray)
+
+	title.value = ''
+	author.value = ''
+	genre.value = ''
+	isbn.value = ''
+})
+
+
+
